7d9cb939cba08fcd2cc3aea350aa2a0b5a410cc7
Student <student@ib.polsl.pl>
2018-02-23 14:32:14 +0100

    3 komit

--- sources/main.cpp
+++ sources/main.cpp
@@ -1,9 +1,25 @@
-#include "trygonometria.h"
+#include<LaborkaConfig.h>
+#ifdef USE_TRIGONOMETRY_DEGREE
+#include <trygonometria.h>
+#else
+#include <cmath>
+#endif
 #include <iostream>
 
-int main() { 
-std::count<<degreemath::sin(90)<<std::endl;
-std::count<<degreemath::cos(90)<<std::endl;
-std::count<<degreemath::tan(90)<<std::endl;
-std::count<<degreemath::ctg(90)<<std::endl;
+int main() {
+double result=0;
+double pi=3.14159;
+ 
+#ifdef USE_TRIGONOMETRY_DEGREE
+result = cos(45.0);
+#else
+result = cos(pi/4.0);
+#endif
+
+/*std::cout<<degreemath::sin(90)<<std::endl;
+std::cout<<degreemath::cos(90)<<std::endl;
+std::cout<<degreemath::tan(90)<<std::endl;
+std::cout<<degreemath::ctg(90)<<std::endl; */
+
+std::cout<<result<<std::endl; 
 }
