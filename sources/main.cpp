56a31d86c7aa83b57fd305040a0e7ad4046b4b04
Student <student@ib.polsl.pl>
2018-02-23 14:02:43 +0100

    2 komit

---
+++ sources/main.cpp
@@ -0,0 +1,9 @@
+#include "trygonometria.h"
+#include <iostream>
+
+int main() { 
+std::count<<degreemath::sin(90)<<std::endl;
+std::count<<degreemath::cos(90)<<std::endl;
+std::count<<degreemath::tan(90)<<std::endl;
+std::count<<degreemath::ctg(90)<<std::endl;
+}
