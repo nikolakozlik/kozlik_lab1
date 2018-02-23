4b5e2836c4027d95c8c0f60575cde5e196032052
Student <student@ib.polsl.pl>
2018-02-23 14:56:34 +0100

    5 komit

--- sources/main.cpp
+++ sources/main.cpp
@@ -5,21 +5,32 @@
 #include <cmath>
 #endif
 #include <iostream>
+#include<string>
 
-int main() {
+int main(int argc,char*argv[]) 
+{
 double result=0;
 double pi=3.14159;
+double angle=45;
+if(argc>1)
+{angle=std::stod(argv[1]);}
+double angle_rad=angle*pi/180.0;
+
  
 #ifdef USE_TRIGONOMETRY_DEGREE
-result = cos(45.0);
-#else
-result = cos(pi/4.0);
-#endif
+std::cout<<degreemath::sin(angle)<<std::endl;
+std::cout<<degreemath::cos(angle)<<std::endl;
+std::cout<<degreemath::tan(angle)<<std::endl;
+std::cout<<degreemath::ctg(angle)<<std::endl; 
 
-/*std::cout<<degreemath::sin(90)<<std::endl;
+#else
+std::cout<<degreemath::sin(90)<<std::endl;
 std::cout<<degreemath::cos(90)<<std::endl;
 std::cout<<degreemath::tan(90)<<std::endl;
-std::cout<<degreemath::ctg(90)<<std::endl; */
+std::cout<<degreemath::ctg(90)<<std::endl; 
+#endif
+
 
 std::cout<<result<<std::endl; 
+system("pause");
 }
